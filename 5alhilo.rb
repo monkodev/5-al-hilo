d4f7b4e674e148b41a0ba97d62ac2d72a449d85e
MonKoDev Technologies <gmalugani@gmail.com>
2016-10-26 19:22:30 -0300

    Actualiza .rb

---
+++ 5alhilo.rb
@@ -0,0 +1,25 @@
+class ManejaTXT
+	def leepregunta
+		
+	end
+	def devpregunta
+		
+	end
+	def leerespuesta
+		
+	end
+	def devuelverespuesta
+		
+	end
+end
+
+class ManejaInterfaz
+ def initialize
+ 	puts "Bienvenido a 5 al Hilo (ACRóNIMO)!!!. Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
+ end	
+
+
+end
+
+
+ManejaInterfaz.initialize
