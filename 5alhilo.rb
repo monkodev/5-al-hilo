b20dd00a104ceceac41b83e121f2947535e29cb0
MonKoDev Technologies <gmalugani@gmail.com>
2016-11-01 13:07:35 -0300

    Version final 5alhilo 1.0.0

--- 5alhilo.rb
+++ 5alhilo.rb
@@ -1,25 +1,68 @@
 class ManejaTXT
-	def leepregunta
-		
-	end
-	def devpregunta
-		
-	end
-	def leerespuesta
-		
-	end
-	def devuelverespuesta
-		
-	end
+	def leetxt	
+		preg=File.open('5alhilo.txt', 'r') 		
+		 preg.readlines
+	end	
 end
 
 class ManejaInterfaz
  def initialize
- 	puts "Bienvenido a 5 al Hilo (ACRóNIMO)!!!. Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
+ 	puts "  "
+ 	puts "--------------------------------------------------------------------------"
+ 	puts "Bienvenido a 5 al Hilo (ACRóNIMO)!!!. Para jugar, solo ingresa el término "
+ 	puts "      correcto para cada una de las definiciones, Listo? Vamos!"
+ 	puts "--------------------------------------------------------------------------"
+ 	puts "-----------------Creado por Giancarlo Malugani----------------------------"
+ 	puts "--------------------------------------------------------------------------"
+ 	puts "  "
+ 	puts "  " 	
  end	
+ def juego_ini
+ 	puts "      ¿¿¿¿¿ Quieres comenzar el desafio??? Presiona (S)-Si / (N)-No"
+ 	puts " "
+ 	op = gets.chomp.capitalize!
+ 	if op=="S"
+ 		arraytx=ManejaTXT.new
+ 		arreglotx=arraytx.leetxt
+ 		cont=0
+ 		for i in 0..4
+ 			re=arreglotx[i*3+1].chomp.upcase	
+ 			puts ""
+			puts "Define: " "#{arreglotx[i*3]}"
+			resp=""
+			while resp!=re 
+				print "Adivinar: "
+				resp=gets.chomp.upcase	
+				if resp!=re 
+					puts "INCORRECTO!! Prueba de nuevo..."
+				else
+					if cont ==4
+						cont=cont+1
+						break
+					end
+					puts "CORRECTO!!!!! Continúa avanzando ..."	
+					cont=cont+1
 
-
+				end			
+			end
+		end 	
+ 	elsif op=="N"
+ 		puts "           Gracias por pasarse por 5 al Hilo!!!! "
+ 			exit
+ 	else
+ 		puts "Opción incorrecta"
+ 		juego_ini
+ 	end
+ 	if cont==5
+ 		puts "---------------------------------------------------------- "
+ 		puts "FELICITACIONES ----GANASTE------HAS RESUELTO 5 AL HILO!!!! "
+ 		puts "---------------------------------------------------------- "
+ 	end
+ end
 end
 
+b=ManejaInterfaz.new
+b.juego_ini
 
-ManejaInterfaz.initialize
+
+
